0b52ec6ce5b9a484bf690cd24f29fb6f10cc1953
Tejal Sonawane <tejalsonawane2423@gmail.com>
2026-03-04 15:00:00 +0530

    adding the event handling of text part1

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,24 +1,71 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText ed2;
+    Button btnp,btnd,btn7,btn8,btn9,btnm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //id identification
+        ed2=findViewById(R.id.text2);
+        btnp=findViewById(R.id.percent);
+        btnd=findViewById(R.id.divide);
+        btn7=findViewById(R.id.seven);
+        btn8=findViewById(R.id.eight);
+        btn9=findViewById(R.id.nine);
+        btnm=findViewById(R.id.multiply);
+
+        //Event Handling
+        btnp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                ed2.setText("%");
+            }
         });
+        btnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed2.setText("/");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed2.setText("7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed2.setText("8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed2.setText("9");
+            }
+        });
+        btnm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed2.setText("x");
+            }
+        });
+
     }
 }

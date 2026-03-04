669acb027cbf20253730e7695696756b067d6a20
Tejal Sonawane <tejalsonawane2423@gmail.com>
2026-03-04 22:30:39 +0530

    adding the event handling of text part5

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ed2.setText(".");
-            }
+             }
         });
     }
 }

b37ebbe2613fd618e35681c2f892225e0e0b6203
Tejal Sonawane <tejalsonawane2423@gmail.com>
2026-03-05 11:44:14 +0530

    add the event handling part 1

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,11 +8,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Defining objects
+    Button btnp,btnd;
+    EditText ed1,ed2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //defining the edittext
+        ed1=findViewById(R.id.text1);
+        ed2= findViewById(R.id.text2);
+
+        //defining the buttons
+        btnp=findViewById(R.id.percent);
+        btnd=findViewById(R.id.divide);
+
     }
 }

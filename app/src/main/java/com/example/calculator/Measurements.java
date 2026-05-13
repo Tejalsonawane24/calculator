57da2a6a809641bca4d1a63185beeb0d2c5b27ca
Tejal Sonawane <tejalsonawane2423@gmail.com>
2026-05-13 16:26:39 +0530

    Unit convertor UI is ready

--- app/src/main/java/com/example/calculator/Measurements.java
+++ app/src/main/java/com/example/calculator/Measurements.java
@@ -1,14 +1,18 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Measurements extends AppCompatActivity {
+    Button mback,mcurrency,mlength,marea,mvolume,mweight,mtemprature,mspeed,mpressure,mpower,mnumsys;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.measurments);
+
+
     }
 
 }

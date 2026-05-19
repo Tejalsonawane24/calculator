785eb3b32c51ce88888c255b3f80301f9139f26f
Tejal Sonawane <tejalsonawane2423@gmail.com>
2026-05-19 12:59:11 +0530

    Currency convertor part1

--- app/src/main/java/com/example/calculator/CurrencyPage.java
+++ app/src/main/java/com/example/calculator/CurrencyPage.java
@@ -1,7 +1,12 @@
 package com.example.calculator;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CurrencyPage extends AppCompatActivity {
@@ -9,5 +14,39 @@ public class CurrencyPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.currency_page);
+      Spinner spinner1=findViewById(R.id.sp1);
+        Spinner spinner2=findViewById(R.id.sp2);
+        EditText edittext1=findViewById(R.id.edcc1);
+        EditText editText2=findViewById(R.id.edcc2);
+
+      ArrayAdapter<CharSequence> adapter1=ArrayAdapter.createFromResource(this, R.array.ccsp1, android.R.layout.simple_spinner_item);
+      ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,R.array.ccsp1, android.R.layout.simple_spinner_item);
+      adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+      spinner1.setAdapter(adapter1);
+      spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+          @Override
+          public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+              String selectedItem =parent.getItemAtPosition(position).toString();
+                edcc1.setText
+          }
+          @Override
+          public void onNothingSelected(AdapterView<?> parent){
+              Toast.makeText(CurrencyPage.this,"Selected:Not Yet",Toast.LENGTH_SHORT).show();
+          }
+      });
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter1);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                String selectedItem =parent.getItemAtPosition(position).toString();
+                Toast.makeText(CurrencyPage.this,"Selected:"+selectedItem,Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+                Toast.makeText(CurrencyPage.this,"Selected:Not Yet",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+

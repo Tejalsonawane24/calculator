f3983b086a08f39e443f21d7826e3eec9a74d597
Tejal Sonawane <tejalsonawane2423@gmail.com>
2026-05-22 00:29:14 +0530

    currency editext is working properly

--- app/src/main/java/com/example/calculator/CurrencyPage.java
+++ app/src/main/java/com/example/calculator/CurrencyPage.java
@@ -14,33 +14,43 @@ public class CurrencyPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.currency_page);
-      Spinner spinner1=findViewById(R.id.sp1);
+        Spinner spinner1=findViewById(R.id.sp1);
         Spinner spinner2=findViewById(R.id.sp2);
         EditText edittext1=findViewById(R.id.edcc1);
         EditText editText2=findViewById(R.id.edcc2);
-
-      ArrayAdapter<CharSequence> adapter1=ArrayAdapter.createFromResource(this, R.array.ccsp1, android.R.layout.simple_spinner_item);
-      ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,R.array.ccsp1, android.R.layout.simple_spinner_item);
-      adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-      spinner1.setAdapter(adapter1);
-      spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-          @Override
-          public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
-              String selectedItem =parent.getItemAtPosition(position).toString();
-                edcc1.setText
-          }
-          @Override
-          public void onNothingSelected(AdapterView<?> parent){
-              Toast.makeText(CurrencyPage.this,"Selected:Not Yet",Toast.LENGTH_SHORT).show();
-          }
-      });
+        final String[] valuesArray= getResources().getStringArray(R.array.currency_value);
+        ArrayAdapter<CharSequence> adapter1=ArrayAdapter.createFromResource(this, R.array.ccsp1, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,R.array.ccsp1, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                String selectedItem1 = valuesArray[position];
+                edittext1.setText(selectedItem1);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+                edittext1.setText("0");
+            }
+        });
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter1);
+        spinner2.setAdapter(adapter2);
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
-                String selectedItem =parent.getItemAtPosition(position).toString();
-                Toast.makeText(CurrencyPage.this,"Selected:"+selectedItem,Toast.LENGTH_SHORT).show();
+                String selectedItem2 = valuesArray[position];
+                try {
+                    // Use double to handle decimal values from strings.xml
+                    double ch1 = Double.parseDouble(selectedItem2);
+                    int pos = spinner1.getSelectedItemPosition();
+                    if (pos != AdapterView.INVALID_POSITION) {
+                        double ch2 = Double.parseDouble(valuesArray[pos]);
+                        editText2.setText(String.valueOf(ch1 * ch2));
+                    }
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent){
@@ -49,4 +59,3 @@ public class CurrencyPage extends AppCompatActivity {
         });
     }
 }
-

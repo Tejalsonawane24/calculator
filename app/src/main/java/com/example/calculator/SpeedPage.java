916f9d2ba3a911b34fdac953c48e8d525bac68e9
Tejal Sonawane <tejalsonawane2423@gmail.com>
2026-06-19 09:50:46 +0530

    Speed is almost done

--- app/src/main/java/com/example/calculator/SpeedPage.java
+++ app/src/main/java/com/example/calculator/SpeedPage.java
@@ -1,6 +1,13 @@
 package com.example.calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +16,31 @@ public class SpeedPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.speed_page);
+        Spinner scsp1 = findViewById(R.id.sps1);
+        Spinner scsp2 = findViewById(R.id.spsc2);
+        EditText sced1 = findViewById(R.id.edsc1);
+        EditText sced2 = findViewById(R.id.edsc2);
+
+        ArrayAdapter<CharSequence> adaptersc1 = ArrayAdapter.createFromResource(this, R.array.spsc, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adaptersc2 = ArrayAdapter.createFromResource(this, R.array.spsc, android.R.layout.simple_spinner_item);
+        adaptersc1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        scsp1.setAdapter(adaptersc1);
+        scsp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, android.view.View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                sced1.setText("0");
+            }
+        });
+
     }
+    public void showMeasurements(View view){
+        Intent i=new Intent(getApplicationContext(), Measurements.class);
+        startActivity(i);
+    }
+
 }
